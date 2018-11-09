b9117e791e1b8122a50ec04887b75911ea7ad170
delboy1978uk <delboy1978uk@gmail.com>
2018-11-09 16:09:19 +0100

    adding expired activation link test

--- tests/api/UserCest.php
+++ tests/api/UserCest.php
@@ -114,6 +114,55 @@ class UserCest
      * @param ApiTester $I
      * @throws Exception
      */
+    public function tryToActivateWithExpiredToken(ApiTester $I)
+    {
+        $email = uniqid() . '@' . uniqid() . '.net';
+        $I->sendPOST('/en_GB/user/register', [
+            'email' => $email,
+            'password' => 'nothing',
+            'confirm' => 'nothing'
+        ]);
+        $token = $I->grabDataFromResponseByJsonPath('$.token');
+
+        $env = new \Bone\Server\Environment([]);
+        $config = $env->fetchConfig(APPLICATION_PATH . '/config', getenv('APPLICATION_ENV'));
+
+        $dbname = $config['db']['database'];
+        $user = $config['db']['user'];
+        $pass = $config['db']['pass'];
+        $host = $config['db']['host'];
+
+        $credentials = new \Del\Common\Config\DbCredentials();
+        $credentials->setUser($user)
+            ->setPassword($pass)
+            ->setDatabase($dbname)
+            ->setHost($host);
+
+        $svc = \Del\Common\ContainerService::getInstance();
+        $svc->setDbCredentials($credentials);
+
+        $package = new \OAuth\OAuthPackage();
+        $svc->registerToContainer($package);
+
+        $container = $svc->getContainer();
+        /** @var \Del\Repository\EmailLink $svc */
+        $svc = $container['repository.EmailLink'];
+        $link = $svc->findByToken($token[0]);
+        $link->setExpiryDate(DateTime::createFromFormat('Y-m-d', '2000-01-01'));
+        $svc->save($link);
+
+        $I->sendGET('/user/activate/' . $email . '/' . $token[0]);
+        $I->seeResponseCodeIs(400);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType([
+            'error' => 'string',
+        ]);
+    }
+
+    /**
+     * @param ApiTester $I
+     * @throws Exception
+     */
     public function tryToSendLostPasswordEmail(ApiTester $I)
     {
         $email = uniqid() . '@' . uniqid() . '.net';
@@ -128,7 +177,7 @@ class UserCest
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200);
         $I->seeResponseMatchesJsonType([
-            "token" => 'string',
+            'token' => 'string',
         ]);
     }
 }
