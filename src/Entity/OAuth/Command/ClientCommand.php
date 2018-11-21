2e28b9dbc7c29992d87afd0daca3d8fea856ea3d
delboy1978uk <delboy1978uk@gmail.com>
2018-11-21 17:18:40 +0100

    update client creation command

--- src/Entity/OAuth/Command/ClientCommand.php
+++ src/Entity/OAuth/Command/ClientCommand.php
@@ -9,6 +9,7 @@ use OAuth\Scope;
 use OAuth\Service\ClientService;
 use OAuth\OAuthUser as User;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
@@ -36,6 +37,12 @@ class ClientCommand extends Command
      */
     private $scopeRepository;
 
+    /** @var QuestionHelper $helper */
+    private $helper;
+
+    /** @var User $user */
+    private $user;
+
     public function __construct(ClientService $clientService, UserService $userService, ScopeRepository $scopeRepository)
     {
         $this->clientService = $clientService;
@@ -53,6 +60,16 @@ class ClientCommand extends Command
         $this->setHelp('Create a new OAuth2 client application');
     }
 
+    private function userQuestion(InputInterface $input, OutputInterface $output)
+    {
+        $question = new Question('Enter the email of the account: ', false);
+        $email = $this->helper->ask($input, $output, $question);
+
+        $this->userService->setUserClass(User::class);
+        /** @var User $user */
+        $this->user = $this->userService->findUserByEmail($email);
+    }
+
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
@@ -61,54 +78,79 @@ class ClientCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Bone API client creator');
-        $helper = $this->getHelper('question');
+        $this->helper = $this->getHelper('question');
 
-        $question = new Question('Enter the email of the account: ', false);
-        $email = $helper->ask($input, $output, $question);
+        $this->userQuestion($input, $output);
 
-        $this->userService->setUserClass(User::class);
-        /** @var User $user */
-        $user = $this->userService->findUserByEmail($email);
-
-        if (!$user) {
+        if (!$this->user) {
             $output->writeln('User not found. Exiting.');
             return null;
         }
 
+        $question = new ConfirmationQuestion('Is this a machine only (client_credentials) API key? ', false);
+        $isClientCredentials = $this->helper->ask($input, $output, $question);
+
+        if ($isClientCredentials) {
+            $authGrant = 'client_credentials';
+            $confidential = true;
+        } else {
+
+            $question = new ChoiceQuestion('What type of app is this? ', [
+                'browser', 'server', 'native'
+            ]);
+            $clientType = $this->helper->ask($input, $output, $question);
+
+            $question = new ConfirmationQuestion('Is this a trusted first party app? ', true);
+            $confidential = (int) $this->helper->ask($input, $output, $question);
+
+            switch ($clientType) {
+                case 'browser':
+                    $authGrant = 'auth_code';
+                    break;
+                case 'useragent':
+                case 'native':
+                    $grants = [
+                        'native' => [
+                            0 => 'auth_code',
+                            1 => 'password',
+                        ],
+                        'useragent' => [
+                            0 => 'implicit',
+                            1 => 'password',
+                        ],
+                    ];
+                    $authGrant = $grants[$clientType][$confidential];
+                    break;
+            }
+        }
+
+        $output->writeln('Setting GrantType to ' . $authGrant . '..');
+        $output->writeln('Setting confidential to ' . ($confidential ? 'true' : 'false') . '..');
+
         $question = new Question('Give a name for this application: ', false);
-        $name = $helper->ask($input, $output, $question);
+        $name = $this->helper->ask($input, $output, $question);
 
         $question = new Question('Give a description: ', false);
-        $description = $helper->ask($input, $output, $question);
+        $description = $this->helper->ask($input, $output, $question);
 
         $question = new Question('Give an icon URL: ', false);
-        $icon = $helper->ask($input, $output, $question);
-
-        $question = new ChoiceQuestion('Select the Authorisation Grant type: ',[
-            'auth_code', 'implicit', 'password', 'client_credentials'
-        ]);
-        $authGrant = $helper->ask($input, $output, $question);
+        $icon = $this->helper->ask($input, $output, $question);
 
         $question = new Question('Give a redirect URI: ', '');
-        $uri = $helper->ask($input, $output, $question);
-
-        $confidential = true;
-        if ($authGrant !== 'client_credentials') {
-            $question = new ConfirmationQuestion('Is this a phone app or JS client ? ', false);
-            $public = $helper->ask($input, $output, $question);
-            $confidential = !$public;
-        }
+        $uri = $this->helper->ask($input, $output, $question);
 
         $scopes = $this->scopeRepository->findAll();
         $choices = [];
         /** @var Scope $scope */
-        foreach($scopes as $index => $scope) {
-            $choices[$index] = $scope->getIdentifier();
+        foreach($scopes as $scope) {
+            $scopeName = $scope->getIdentifier();
+            $choices[] = $scopeName;
+            $scopes[$scopeName] = $scope;
         }
 
         $question = new ChoiceQuestion('Which scopes would you like to add?', $choices);
         $question->setMultiselect(true);
-        $scopeChoices = $helper->ask($input, $output, $question);
+        $scopeChoices = $this->helper->ask($input, $output, $question);
 
         $client = new Client();
         $client->setName($name);
@@ -118,13 +160,16 @@ class ClientCommand extends Command
         $client->setIdentifier(md5($name));
         $client->setRedirectUri($uri);
         $client->setConfidential($confidential);
-        $client->setUser($user);
-        foreach ($scopeChoices as $scopeIndex => $name) {
-            $scope = $scopes[$scopeIndex];
+        $client->setUser($this->user);
+
+        foreach ($scopeChoices as $name) {
+            $output->writeln('Registering ' . $name . ' scope with client..');
+            $scope = $scopes[$name];
             $client->getScopes()->add($scope);
         }
 
         if ($confidential) {
+            $output->writeln('Generating client secret..');
             $this->clientService->generateSecret($client);
         }
 
