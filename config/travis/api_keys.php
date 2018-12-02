0c658187f833dc397e93f90231d9093bc8661428
delboy1978uk <delboy1978uk@gmail.com>
2018-12-02 17:08:03 +0100

    use port 8443 for ssl

--- config/travis/api_keys.php
+++ config/travis/api_keys.php
@@ -5,9 +5,9 @@ return [
         'clientCredentials' => [
             'clientId' => 'ceac682a9a4808bf910ad49134230e0e',
             'clientSecret' => 'JDJ5JDEwJGNEd1J1VEdOY0YxS3QvL0pWQzMxay52',
-            'urlAccessToken' => 'https://awesome.scot:8000/oauth2/access-token',
+            'urlAccessToken' => 'https://awesome.scot:8443/oauth2/access-token',
             'urlResourceOwnerDetails' => '/me',
-            'host' => 'https://awesome.scot:8000',
+            'host' => 'https://awesome.scot:8443',
             'redirectUri' => '',
             'urlAuthorize' => 'https://not-used-with-this-grant',
             'verify' => false,
