c7c413174de1b61d628f19d82ea70821a8def5db
delboy1978uk <delboy1978uk@gmail.com>
2018-12-02 03:19:09 +0100

    hopefully travis is now on SSL

--- config/travis/api_keys.php
+++ config/travis/api_keys.php
@@ -7,7 +7,7 @@ return [
             'clientSecret' => 'JDJ5JDEwJGNEd1J1VEdOY0YxS3QvL0pWQzMxay52',
             'urlAccessToken' => 'https://awesome.scot:8000/oauth2/access-token',
             'urlResourceOwnerDetails' => '/me',
-            'host' => 'https://awesome.scot',
+            'host' => 'https://awesome.scot:8000',
             'redirectUri' => '',
             'urlAuthorize' => 'https://not-used-with-this-grant',
             'verify' => false,
