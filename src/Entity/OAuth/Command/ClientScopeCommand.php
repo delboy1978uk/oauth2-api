6c1ddd9dbaa5fcfc0764f6c4c330507acc8f2fda
delboy1978uk <delboy1978uk@gmail.com>
2018-12-02 21:26:10 +0100

    addin client scope command basic args

--- src/Entity/OAuth/Command/ClientScopeCommand.php
+++ src/Entity/OAuth/Command/ClientScopeCommand.php
@@ -8,6 +8,7 @@ use OAuth\Scope;
 use OAuth\Service\ClientService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
@@ -38,6 +39,9 @@ class ClientScopeCommand extends Command
         $this->clientService = $clientService;
         $this->scopeRepository = $scopeRepository;
         parent::__construct('client:scope');
+        $this->addArgument('operation', InputArgument::REQUIRED, 'list, add, or remove.');
+        $this->addArgument('scope', InputArgument::OPTIONAL, 'The scope name when adding or removing.');
+        $this->addArgument('client', InputArgument::OPTIONAL, 'The client identifier.');
     }
 
     /**
@@ -57,8 +61,44 @@ class ClientScopeCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Client scope administration');
-        $this->helper = $this->getHelper('question');
+        $operation = $input->getArgument('operation');
+        switch ($operation) {
+            case 'list';
+                $this->listScopes($input, $output);
+                break;
+            case 'add';
+                $this->addScope($input, $output);
+                break;
+            case 'remove';
+                $this->removeScope($input, $output);
+                break;
+        }
+    }
 
-        $output->writeln('Code something!');
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function listScopes(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('List scopes for client');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function addScope(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Add scope for client');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function removeScope(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Remove scope for client');
     }
 }
